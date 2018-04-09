53367408fc649955d2f676f1d71e872220e57365
Ulrik Günther <ulrik@42degreesoffreedom.com>
2018-04-09 19:01:47 +0200

    Add multMatrixFMA and multMatrixFMALoop benchmark variants that seem to provide a speedup

--- src/main/java/is/ulrik/autovectorisationtest/MultiplyMatrices.java
+++ src/main/java/is/ulrik/autovectorisationtest/MultiplyMatrices.java
@@ -107,4 +107,103 @@ public class MultiplyMatrices {
       }
     }
   }
+
+  @Benchmark
+  public void multiplyMatricesFloatUtilFMA(Context context) {
+    multMatrixFMA(context.A.getFloatArray(), context.B.getFloatArray(), context.C.getFloatArray());
+  }
+
+  @Benchmark
+  public void multiplyMatricesFloatUtilFMALoop(Context context) {
+    multMatrixFMALoop(context.A.getFloatArray(), context.B.getFloatArray(), context.C.getFloatArray());
+  }
+
+  public static float[] multMatrixFMA(final float[] a, final float[] b, final float[] d) {
+    final float b00 = b[0+0*4];
+    final float b10 = b[1+0*4];
+    final float b20 = b[2+0*4];
+    final float b30 = b[3+0*4];
+    final float b01 = b[0+1*4];
+    final float b11 = b[1+1*4];
+    final float b21 = b[2+1*4];
+    final float b31 = b[3+1*4];
+    final float b02 = b[0+2*4];
+    final float b12 = b[1+2*4];
+    final float b22 = b[2+2*4];
+    final float b32 = b[3+2*4];
+    final float b03 = b[0+3*4];
+    final float b13 = b[1+3*4];
+    final float b23 = b[2+3*4];
+    final float b33 = b[3+3*4];
+
+    float ai0=a[  0*4]; // row-0 of a
+    float ai1=a[  1*4];
+    float ai2=a[  2*4];
+    float ai3=a[  3*4];
+    d[ 0*4] = Math.fma(ai0, b00, Math.fma(ai1, b10, Math.fma(ai2, b20, ai3*b30)));
+    d[ 1*4] = Math.fma(ai0, b01, Math.fma(ai1, b11, Math.fma(ai2, b21, ai3*b31)));
+    d[ 2*4] = Math.fma(ai0, b02, Math.fma(ai1, b12, Math.fma(ai2, b22, ai3*b32)));
+    d[ 3*4] = Math.fma(ai0, b03, Math.fma(ai1, b13, Math.fma(ai2, b23, ai3*b33)));
+
+    ai0=a[1+0*4]; // row-1 of a
+    ai1=a[1+1*4];
+    ai2=a[1+2*4];
+    ai3=a[1+3*4];
+    d[1+0*4] = Math.fma(ai0, b00, Math.fma(ai1, b10, Math.fma(ai2, b20, ai3*b30)));
+    d[1+1*4] = Math.fma(ai0, b01, Math.fma(ai1, b11, Math.fma(ai2, b21, ai3*b31)));
+    d[1+2*4] = Math.fma(ai0, b02, Math.fma(ai1, b12, Math.fma(ai2, b22, ai3*b32)));
+    d[1+3*4] = Math.fma(ai0, b03, Math.fma(ai1, b13, Math.fma(ai2, b23, ai3*b33)));
+
+    ai0=a[2+0*4]; // row-2 of a
+    ai1=a[2+1*4];
+    ai2=a[2+2*4];
+    ai3=a[2+3*4];
+    d[2+0*4] = Math.fma(ai0, b00, Math.fma(ai1, b10, Math.fma(ai2, b20, ai3*b30)));
+    d[2+1*4] = Math.fma(ai0, b01, Math.fma(ai1, b11, Math.fma(ai2, b21, ai3*b31)));
+    d[2+2*4] = Math.fma(ai0, b02, Math.fma(ai1, b12, Math.fma(ai2, b22, ai3*b32)));
+    d[2+3*4] = Math.fma(ai0, b03, Math.fma(ai1, b13, Math.fma(ai2, b23, ai3*b33)));
+
+    ai0=a[3+0*4]; // row-3 of a
+    ai1=a[3+1*4];
+    ai2=a[3+2*4];
+    ai3=a[3+3*4];
+    d[3+0*4] = Math.fma(ai0, b00, Math.fma(ai1, b10, Math.fma(ai2, b20, ai3*b30)));
+    d[3+1*4] = Math.fma(ai0, b01, Math.fma(ai1, b11, Math.fma(ai2, b21, ai3*b31)));
+    d[3+2*4] = Math.fma(ai0, b02, Math.fma(ai1, b12, Math.fma(ai2, b22, ai3*b32)));
+    d[3+3*4] = Math.fma(ai0, b03, Math.fma(ai1, b13, Math.fma(ai2, b23, ai3*b33)));
+
+    return d;
+  }
+
+  public static float[] multMatrixFMALoop(final float[] a, final float[] b, final float[] d) {
+    final float b00 = b[0+0*4];
+    final float b10 = b[1+0*4];
+    final float b20 = b[2+0*4];
+    final float b30 = b[3+0*4];
+    final float b01 = b[0+1*4];
+    final float b11 = b[1+1*4];
+    final float b21 = b[2+1*4];
+    final float b31 = b[3+1*4];
+    final float b02 = b[0+2*4];
+    final float b12 = b[1+2*4];
+    final float b22 = b[2+2*4];
+    final float b32 = b[3+2*4];
+    final float b03 = b[0+3*4];
+    final float b13 = b[1+3*4];
+    final float b23 = b[2+3*4];
+    final float b33 = b[3+3*4];
+
+    for(int i = 0; i < 4; i++) {
+      float ai0 = a[i+0 * 4]; // row-0 of a
+      float ai1 = a[i+1 * 4];
+      float ai2 = a[i+2 * 4];
+      float ai3 = a[i+3 * 4];
+      d[i+0 * 4] = Math.fma(ai0, b00, Math.fma(ai1, b10, Math.fma(ai2, b20, ai3 * b30)));
+      d[i+1 * 4] = Math.fma(ai0, b01, Math.fma(ai1, b11, Math.fma(ai2, b21, ai3 * b31)));
+      d[i+2 * 4] = Math.fma(ai0, b02, Math.fma(ai1, b12, Math.fma(ai2, b22, ai3 * b32)));
+      d[i+3 * 4] = Math.fma(ai0, b03, Math.fma(ai1, b13, Math.fma(ai2, b23, ai3 * b33)));
+    }
+
+    return d;
+  }
 }
