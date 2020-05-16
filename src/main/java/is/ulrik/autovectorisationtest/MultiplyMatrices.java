4c7b413ac9c8244624f3c7df55005aa289cf428d
Ulrik Guenther <hello@ulrik.is>
2020-05-16 16:42:28 +0200

    Use time-based evaluation

--- src/main/java/is/ulrik/autovectorisationtest/MultiplyMatrices.java
+++ src/main/java/is/ulrik/autovectorisationtest/MultiplyMatrices.java
@@ -38,6 +38,7 @@ import org.openjdk.jmh.annotations.*;
 import java.util.Random;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.openjdk.jmh.annotations.Mode.AverageTime;
 
 @State(Scope.Thread)
@@ -45,10 +46,10 @@ import static org.openjdk.jmh.annotations.Mode.AverageTime;
 @BenchmarkMode(AverageTime)
 @Fork(value = 1, jvmArgsAppend = {
         "-XX:-UseSuperWord",
-        "-XX:+UnlockDiagnosticVMOptions",
-        "-XX:CompileCommand=print,*BenchmarkSIMDBlog.array1"})
+        "-XX:+UnlockDiagnosticVMOptions"/*,
+        "-XX:CompileCommand=print,*BenchmarkSIMDBlog.array1"*/})
 @Warmup(iterations = 5)
-@Measurement(iterations = 10)
+@Measurement(time = 5, timeUnit = SECONDS, iterations = 5)
 public class MultiplyMatrices {
 
   @State(Scope.Thread)
